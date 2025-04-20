a73c43881d9b36d4b0473216e9a06ad0646407db
onlytomorrow <vincent@onlytomorrow.net>
2025-04-20 15:29:22 -0500

    chore(astro): bump packages
    added svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,14 @@ import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel";
 import swup from '@swup/astro';
 
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
 export default defineConfig({
     integrations: [swup({
         theme:'fade',
         containers: ['#swup', '#heading'],
-    })],
+    }), svelte()],
     output: "static",
     adapter: vercel()
 });
