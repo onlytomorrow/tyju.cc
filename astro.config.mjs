d289b428d762f385bcad2d9f65a32e821beb3227
onlytomorrow <vincent@onlytomorrow.net>
2025-04-20 14:25:30 -0500

    chore(astro): bump packages
    @astrojs/vercel/serverless is deprecated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from 'astro/config';
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 import swup from '@swup/astro';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [swup({
-    theme:'fade',
-    containers: ['#swup', '#heading'],
-  })],
-  output: "server",
-  adapter: vercel()
+    integrations: [swup({
+        theme:'fade',
+        containers: ['#swup', '#heading'],
+    })],
+    output: "static",
+    adapter: vercel()
 });
